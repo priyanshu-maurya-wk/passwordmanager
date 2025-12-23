1db75fab91ccc67c62720d104d61ca3c5d80edf8
Priyanshu <priyanshumauryacontact@gmail.com>
2025-12-23 18:52:43 +0530

    added encryption and decryption to main code

--- new.py
+++ new.py
@@ -1,12 +1,18 @@
-from cryptography.fernet import fernet
+from cryptography.fernet import Fernet
 
 while True:
+    
     user = input("Do you want to decrypt passowrd or store a passowrd (1/2): ")
-
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    
     if user == "1":
-        pass
+        decoded_text = cipher_suite.decrypt(user).decode()
+        print(decoded_text)
+        
     elif user == "2":
         textfile = open("text.txt", "a")
         textfile.write(input())
+        
     else:
-        print("Invalid choice! Your passowrd is now leaked to internet with all of your information if you stored password before you dumb bitch")
+        print("Invalid choice!")
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 user = input("Enter whatever: ")
-key = Fernet.generate_key() #this is your "password"
+key = Fernet.generate_key()
 cipher_suite = Fernet(key)
 encoded_text = cipher_suite.encrypt(user.encode())
 print(encoded_text)
