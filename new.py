ec9c477966c4e78c89f278a5868278c2bd5163ce
Priyanshu <priyanshumauryacontact@gmail.com>
2025-12-23 19:19:55 +0530

    final trash

--- new.py
+++ new.py
@@ -1,18 +1,22 @@
 from cryptography.fernet import Fernet
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
 while True:
     
     user = input("Do you want to decrypt passowrd or store a passowrd (1/2): ")
-    key = Fernet.generate_key()
-    cipher_suite = Fernet(key)
     
-    if user == "1":
-        decoded_text = cipher_suite.decrypt(user).decode()
-        print(decoded_text)
-        
-    elif user == "2":
+    if user == "2":
         textfile = open("text.txt", "a")
-        textfile.write(input())
+        password = input("Enter the password: ")
+        encoded_text = cipher_suite.encrypt(password.encode())
+        textfile.write(password)
+        print(encoded_text)
         
+    elif user == "1":
+        decryptkey = input("Enter the key to decrypt it: ")
+        decoded_text = cipher_suite.decrypt(decryptkey).decode()
+        print(decoded_text)
+         
     else:
         print("Invalid choice!")
